44e04389c4712c33a1c5094642180cbf8ddea4f7
Mayara Rocha <mayaraaraujorocha54@gmail.com>
2022-09-14 16:10:21 -0300

    adiconando construtores,atributos estáticos,encapsulamento e atributos readonly

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,13 @@
 export class Cliente { //campos e atributos
   nome;
-  cpf;
-   }
+  _cpf;
+
+  get cpf() {
+   return this._cpf;
+}
+
+constructor(nome, cpf) {
+    this.nome =  nome;
+    this._cpf = cpf; 
+}
+ }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,41 +1,50 @@
-import {Cliente} from './Cliente.js'
+import { Cliente } from './Cliente.js'
 export class ContaCorrente {
+ static numeroDeContas = 0;//atributo estatico
+
   agencia;
   _cliente;
 
 
-  set cliente(novoValor){
-    if(novoValor instanceof Cliente){//se meu novo valor for uma isntancia de cliente se sim faça a atribuição se não não faça nada para não atribuir um valor errado
-       this._cliente = novoValor;
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {//se meu novo valor for uma isntancia de cliente se sim faça a atribuição se não não faça nada para não atribuir um valor errado
+      this._cliente = novoValor;
     }
-   
+
   }
-  get cliente(){ 
+  get cliente() {
     return this._cliente;
   }
 
   _saldo = 0;
- get saldo(){ //get é um acessor publico
- this._saldo;
- }
-  sacar(valor){//é uma operação com valor
-   if(this._saldo >= valor){
-     this._saldo -= valor
-     return valor;// se não tiver o return vai me devolver undefined
-   }
+  get saldo() { //get é um acessor publico
+    this._saldo;
+  }
+
+  constructor(agencia, cliente) {
+    this.agencia = agencia;
+    this.cliente = cliente;
+    ContaCorrente.numeroDeContas += 1;
+  }
+
+  sacar(valor) {//é uma operação com valor
+    if (this._saldo >= valor) {
+      this._saldo -= valor
+      return valor;// se não tiver o return vai me devolver undefined
+    }
 
   }
-  depositar(valor){
-     if(valor <= 0){ //pegar o valor verificar se não está negativo
+  depositar(valor) {
+    if (valor <= 0) { //pegar o valor verificar se não está negativo
       return;// o retorno de um método (return) pode ter duas funções: parar a execução antecipadamente ao nos depararmos com uma condição indesejada, algo que chamamos de early return; ou realmente retornar um valor para que o sistema continue trabalhando com ele de alguma forma.        
-     }
-   this._saldo += valor;// fazer a soma do valor só se for positivo
+    }
+    this._saldo += valor;// fazer a soma do valor só se for positivo
   }
 
-  transferir(valor, conta){
+  transferir(valor, conta) {
     conta.cidade = "São Paulo";//adicionando dinamicamente um atributo
     const valorSacado = this.sacar(valor);
     conta.depositar(valorSacado);
-   
+
   }
 }
